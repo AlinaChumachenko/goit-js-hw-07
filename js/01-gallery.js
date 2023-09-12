909571d4baecda2187904ae8afe3cc881f49d191
AlinaChumachenko <103441578+AlinaChumachenko@users.noreply.github.com>
2023-09-12 16:58:11 +0300

    HW_7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector(".gallery");
+const markup = galleryItems.map(
+    ({preview, original, description}) => ` <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+         class="gallery__image"
+         src="${preview}"
+         data-source="${original}"
+         alt="${description}"
+       />
+     </a>
+   </li> `
+).join("");
+list.insertAdjacentHTML("beforeend", markup)
 
 console.log(galleryItems);
+list.addEventListener("click", onPictureClick);
+
+function onPictureClick (evt) {
+  evt.preventDefault();
+  if(evt.target === evt.currentTarget){
+      return;}
+      // console.log(evt.target, evt.currentTarget);
+       
+      // console.log(evt.target.closest('.gallery')); 
+      const originalImg = evt.target.dataset.source;
+
+const instance = basicLightbox.create(`<div class = "modal">
+<img
+      class="gallery__image"
+      src="${originalImg}"
+      width="1000"
+    />
+</div>`);
+instance.show();
+
+document.addEventListener("keydown", onHandleKeyDown);
+
+function onHandleKeyDown(evt) {
+  if (evt.code === "Escape") { 
+    instance.close();}
+}
+
+};
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const list = document.querySelector(".gallery");
+const markup = galleryItems.map(({preview, original, description}) => 
+     `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>`
+).join("");
+list.insertAdjacentHTML("beforeend", markup)
 console.log(galleryItems);
+
+list.addEventListener("click", onPictureClick);
+
+function onPictureClick (evt) {
+  evt.preventDefault();
+  if(evt.target === evt.currentTarget){
+      return;}
+  
+    new SimpleLightbox (".gallery a", {
+        captionsData: "alt", 
+        captionDelay: 250
+    }
+);    
+  }
